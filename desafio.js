356e9417ac12195572bdc4c77c369a91fad677df
Paula Yunes <pauliyunes94@gmail.com>
2022-01-16 19:32:03 -0300

    Primer commit: Desafio 3

---
+++ desafio.js
@@ -0,0 +1,70 @@
+const {promises: fs} = require('fs');
+
+class Contenedor{
+    constructor(archivo){
+        this.archivo = archivo;
+    }
+
+    async save(obj){
+        const objs = await this.getAll()
+        let newId 
+        if (objs.length == 0){
+            newId = 1
+        } else{
+            newId = objs[objs.length - 1].id +1
+        }
+        const newObj = {...obj, id: newId}
+        objs.push(newObj)
+        try{
+            await fs.writeFile(this.archivo,JSON.stringify(objs,null,4));
+            return newId;
+        } catch(err){
+            console.log('Error',err)
+        }
+    }
+
+    async getById(id){
+        const objs = await this.getAll()
+        const buscarId= objs.find(obs => obs.id == id)
+        return buscarId
+    }
+
+    async getAll(){
+        try{
+            const objs = await fs.readFile(this.archivo,'utf-8')
+            return JSON.parse(objs)
+        }
+        catch(err){
+            return []
+        }
+    }
+
+    async deleteById(id){
+        try {
+            const cont = await fs.readFile(this.archivo, 'utf-8');
+
+            if (cont === "") {
+                return "Empty list";
+            } else {
+                const products = JSON.parse(cont);
+                const newProducts = products.filter((item) => item.id !== id)
+                await fs.writeFile(this.archivo, JSON.stringify(newProducts));
+            }
+        } catch (err) {
+            console.log('Error',err)
+        }
+    }
+    
+
+    async deleteAll(){
+        try{
+            await fs.promises.writeFile(`./${this.archivo.toString()}.txt`,"")
+            console.log("Se elimino el contenido")
+        }
+        catch(err){
+            console.log('Error',err)
+        }
+    }
+}
+
+module.exports = Contenedor;
---
+++ desafio3.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 8080 || process.env.port;
+
+const Contenedor = require('./desafio.js')
+const prods = new Contenedor('./productos.txt')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Array de productos
+const productos = [
+    {
+        title: 'Cartuchera',
+        price: 1200,
+        imgurl: 'https://http2.mlstatic.com/D_NQ_NP_996400-MLA32738055325_112019-O.jpg',
+        id: 1
+    },
+    {
+        title: 'Resaltador Stabilo Pastel',
+        price: 980,
+        imgurl: 'https://http2.mlstatic.com/D_NQ_NP_645415-MLA46333170733_062021-O.jpg',
+        id: 2
+    },
+    {
+        title: 'Cuaderno Cherhane A5',
+        price: '2300',
+        imgurl: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/891/147/products/image0-21-5e2edd3aa8137d541316216162123526-1024-1024.jpeg',
+        id: 3
+    }
+]
+
+app.get('/productos', (req, res) => {
+    const productosJs = JSON.stringify(productos)
+    res.send(`Productos en el servidor: ${productosJs}`)
+})
+
+app.get('/productoRandom', (req, res) => {
+    let rand = Math.floor(Math.random()*productos.length);
+    let rValue = productos[rand];
+    const productosJs = JSON.stringify(rValue)
+    res.send(`Produsto elegido al azar: ${productosJs}`)
+})
+
+app.get('/productostxt', (req, res) => {
+    res.json({
+        productos:prods.getAll()
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening at ${port}`);
+});
